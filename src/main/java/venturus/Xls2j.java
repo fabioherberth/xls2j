751c53b49c9f18181947312e3348eb6b31d81258
Jackson Teixeira <jackson.s.teixeira@gmail.com>
2022-11-05 19:56:34 -0300

    feat: dynamic object instantiation

--- src/main/java/venturus/Xls2j.java
+++ src/main/java/venturus/Xls2j.java
@@ -1,6 +1,8 @@
 package venturus;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,17 +13,13 @@ public final class Xls2j {
     private static final String NO_ANNOTATION_MESSAGE = "%s will be ignored because doesn't have @Sheet annotation";
     private List<Object> sheets;
 
-    public Xls2j() {
-        this.sheets = new ArrayList<>();
-    }
-
-    public List<Object> load(String filename) throws Exception {
+    public List<Object> parse(String filename) throws Exception {
         Loader loader = new ExcelLoader();
 
         try {
             loader.load(filename);
 
-            List<Object> result = this.parse(loader);
+            List<Object> result = this.parseSheets(loader);
 
             return result;
         } finally {
@@ -29,39 +27,57 @@ public final class Xls2j {
         }
     }
 
-    private List<Object> parse(final Loader loader) {
+    private List<Object> parseSheets(final Loader loader) {
         List<Object> result = new ArrayList<>();
 
         for (final Object sheet : sheets) {
-            final Class<?> cls = sheet.getClass();
+            final Class<?> sheetClass = sheet.getClass();
 
-            if (!cls.isAnnotationPresent(Sheet.class)) {
-                Logger.error(String.format(NO_ANNOTATION_MESSAGE, cls.getSimpleName()));
+            if (!sheetClass.isAnnotationPresent(Sheet.class)) {
+                Logger.error(String.format(NO_ANNOTATION_MESSAGE, sheetClass.getSimpleName()));
                 continue;
             }
 
-            Sheet config = cls.getAnnotation(Sheet.class);
-            Logger.debug(String.format("%s -> Tipo %s", config.value(), cls.getSimpleName()));
+            Sheet sheetConfig = sheetClass.getAnnotation(Sheet.class);
+            Logger.debug(String.format("%s -> Tipo %s", sheetConfig.value(), sheetClass.getSimpleName()));
 
-            if (!loader.getSheetsNames().contains(config.value())) {
+            if (!loader.getSheetsNames().contains(sheetConfig.value())) {
                 throw new SheetNotFoundException();
             }
 
-            for (final Field field : cls.getDeclaredFields()) {
-                field.setAccessible(true);
-
-                if (field.isAnnotationPresent(Column.class)) {
-                    Column column = field.getAnnotation(Column.class);
-
-                    Logger.debug(String.format("\t%s -> %s", field.getName(), column.name()));
-                }
-            }
+            Object parsedSheet = parseSheet(sheetClass);
+            result.add(parsedSheet);
         }
 
         return result;
     }
 
+    private Object parseSheet(final Class<?> sheetClass) {
+        for (final Field field : sheetClass.getDeclaredFields()) {
+            field.setAccessible(true);
+
+            if (field.isAnnotationPresent(Column.class)) {
+                Column column = field.getAnnotation(Column.class);
+
+                Logger.debug(String.format("\t%s -> %s", field.getName(), column.name()));
+            }
+        }
+
+        try {
+            return sheetClass.getConstructor().newInstance();
+        } catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException
+                | IllegalArgumentException | InvocationTargetException e) {
+            Logger.error(e.getMessage());
+        }
+
+        return null;
+    }
+
     public Xls2j addSheet(Object sheet) {
+        if (this.sheets == null) {
+            this.sheets = new ArrayList<>();
+        }
+
         sheets.add(sheet);
         return this;
     }
--- src/test/java/venturus/ApplicationTest.java
+++ src/test/java/venturus/ApplicationTest.java
@@ -1,18 +1,22 @@
 package venturus;
 
-// import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
 
 import org.junit.Test;
 
 public class ApplicationTest {
     @Test
     public void shouldParseTest() throws Exception {
-        Xls2j parse = new Xls2j();
+        Xls2j parser = new Xls2j();
 
-        parse
-            .addSheet(new Planilha1())
-            .addSheet(new Planilha2())
-            .addSheet(new Planilha3())
-            .load("/tmp/arquivo.xlsx");
+        List<Object> sheets = parser
+                .addSheet(new Planilha1()) // OK
+                .addSheet(new Planilha2()) // OK
+                .addSheet(new Planilha3()) // Dont parse because object doesn't have @Sheet annotation
+                .parse("/tmp/arquivo.xlsx");
+
+        assertEquals(2, sheets.size());
     }
 }
